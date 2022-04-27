9d26d4c44d7240d771e3a2b99f6f081d030e201e
danieldenton <danielmdenton@gmail.com>
2022-04-27 14:45:36 -0700

    heroku return attempt

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/dcd";
+const MONGODB_URI = process.env.MONGODB_URI;
+// || "mongodb://127.0.0.1/dcd";
 
 mongoose.connect(MONGODB_URI);
 

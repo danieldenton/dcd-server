ba166d1a410314d75673b3b358f76b21b6b3c30e
danieldenton <danielmdenton@gmail.com>
2022-06-03 08:57:53 -0700

    saved

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
+const middleWare = (req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
     "https://deepcutdiscovery.netlify.app"
@@ -20,7 +20,9 @@ app.use((req, res, next) => {
   );
   console.log(`incoming request: ${req.method} - ${req.url}`);
   next();
-});
+};
+
+app.use(middleWare);
 
 app.get("/", (req, res) => {
   res.json({ msg: "Welcome to Deep Cut Discovery" });
